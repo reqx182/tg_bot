8fc127669cecbf3f7435b80b7274ee2d2cdc74c8
reqx182 <96117929+reqx182@users.noreply.github.com>
2022-06-08 21:08:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,162 +1,149 @@
+#!/usr/bin/env python
+# pylint: disable=C0116,W0613
+# This program is dedicated to the public domain under the CC0 license.
+
+"""
+First, a few callback functions are defined. Then, those functions are passed to
+the Dispatcher and registered at their respective places.
+Then, the bot is started and runs until we press Ctrl-C on the command line.
+
+Usage:
+Example of a bot-user conversation using ConversationHandler.
+Send /start to initiate the conversation.
+Press Ctrl-C on the command line or send a signal to the process to stop the
+bot.
+"""
+
 import logging
-import pytube
-import os
-import requests
+from typing import Dict
 
-#os.environ["IMAGEIO_FFMPEG_EXE"] = "/usr/bin/ffmpeg" 2342
-from moviepy.editor import AudioFileClip
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
-from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext, MessageHandler, Filters, \
-    ConversationHandler
-from telegram.constants import MESSAGEENTITY_URL as URL
-from telegram.constants import MESSAGEENTITY_TEXT_LINK as TEXT_LINK
-import telegram.ext.filters as filters
+from telegram import ReplyKeyboardMarkup, Update, ReplyKeyboardRemove
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    MessageHandler,
+    Filters,
+    ConversationHandler,
+    CallbackContext,
+)
 
+# Enable logging
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
 )
+
 logger = logging.getLogger(__name__)
 
-KEYBOARD, BUTTONS, LINK = range(3)
+CHOOSING, TYPING_REPLY, TYPING_CHOICE = range(3)
+
+reply_keyboard = [
+    ['Age', 'Favourite colour'],
+    ['Number of siblings', 'Something else...'],
+    ['Done'],
+]
+markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True) # клава 
 
 
-def start(update: Update, context: CallbackContext):
-    update.message.reply_text('Введите ссылку ')
-    return KEYBOARD
+def facts_to_str(user_data: Dict[str, str]) -> str:
+	
+    """Получаем все инфу введеную пользователем"""
+    facts = [f'{key} - {value}' for key, value in user_data.items()]	 
+    print(facts)
+    return "\n".join(facts).join(['\n', '\n'])
 
 
-def keyboard(update: Update, context: CallbackContext):
-    url = update.message.text
-    """Выводит клавиатуру Загрузить видео | Загрузить аудио"""
-    keyboard = [[
-        InlineKeyboardButton("video", callback_data='video'),
-        InlineKeyboardButton("audio", callback_data='audio')],
-        [
-            InlineKeyboardButton("cancel", callback_data='cancel')
-        ]]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    update.message.reply_text(url, reply_markup=reply_markup)
-    return BUTTONS
-
-
-def link_check(link) -> bool:
-    try:
-        pytube.YouTube(link)
-        return True
-    except:
-        print("Неправильная ссылка")
-        return False
-
-
-def delet_syms(title: str) -> str:
-    """Удаляет лишние символы из названия файла во избежании ошибки при сохранении"""
-    wrong_syms = '|\\/_-!"№;%:?*+='
-    title = title.split()
-    for index in range(len(title)):
-        if title[index] in wrong_syms:
-            title[index] = ''
-    title = "".join(title)
-    return title
-
-
-def video(update: Update, context: CallbackContext):
-    chat_id = update.effective_chat.id
-    link = update.callback_query['message']['text']
-    if not link_check(link):
-        update.callback_query.bot.send_message(chat_id, 'Неправильная ссылка. Введите ссылку ещё раз.')
-        return KEYBOARD
-
-    update.callback_query.bot.send_message(chat_id, 'Подождите, видео скачивается.')
-    yt = pytube.YouTube(link)
-    stream = yt.streams.get_highest_resolution()
-    title = yt.title
-    title = delet_syms(title)
-    filename = f'{title}.mp4'
-    stream.download(filename=filename)
-    file = open(filename, 'rb')
-    update.callback_query.bot.send_video(chat_id, file, timeout = 100)
-    print(os.getcwd())
-    #os.remove(f'{os.getcwd()}/{filename}')
-    print('video')
-
-    return KEYBOARD
-
-
-def audio(update: Update, context: CallbackContext):
-    chat_id = update.effective_chat.id
-    link = update.callback_query['message']['text']
-    if not link_check(link):
-        update.callback_query.bot.send_message(chat_id, 'Неправильная ссылка. Введите ссылку ещё раз.')
-        return KEYBOARD
-
-    update.callback_query.bot.send_message(chat_id, 'Подождите, аудио скачивается.')
-    yt = pytube.YouTube(link)
-    stream = yt.streams.get_highest_resolution()
-    title = yt.title
-    title = delet_syms(title)
-    filename = f'{title}'
-    stream.download(filename=filename)
-    file = open(filename, 'rb')
-    update.callback_query.bot.send_audio(chat_id, file, timeout = 100)
-    convert_to_mp3(filename + '.mp4', filename + '.mp3')
-    os.remove(f'{os.getcwd()}/{filename}')
-    print('audio')
-
-    return KEYBOARD
-
-
-def convert_to_mp3(input_mp4_file, output_name_file='без названия.mp3', delete_input_file=True):
-    in_audio = AudioFileClip(input_mp4_file)
-    in_audio.write_audiofile(filename=output_name_file)
-    in_audio.close()
-    print('конвертация выполнена')
-    if delete_input_file:
-        os.remove(input_mp4_file)
-
-
-def cancel(update: Update, context: CallbackContext):
-    update.message.reply_text('Введите новую ссылку')
-    return KEYBOARD
-
-
-def exit(update: Update, context: CallbackContext):
-    user = update.message.from_user
-    logger.info("User %s canceled the conversation.", user.first_name)
+def start(update: Update, context: CallbackContext) -> int:
     update.message.reply_text(
-        'Bye! I hope we can talk again some day.'
+        "Hi! My name is Doctor Botter. I will hold a more complex conversation with you. "
+        "Why don't you tell me something about yourself?",
+        reply_markup=markup,
     )
+
+    return CHOOSING
+
+
+def regular_choice(update: Update, context: CallbackContext) -> int:
+    """Ask the user for info about the selected predefined choice."""
+    text = update.message.text
+    context.user_data['choice'] = text
+    update.message.reply_text(f'Your {text.lower()}? Yes, I would love to hear about that!')
+
+    return TYPING_REPLY
+
+
+def custom_choice(update: Update, context: CallbackContext) -> int:
+    """Ask the user for a description of a custom category."""
+    update.message.reply_text(
+        'Alright, please send me the category first, for example "Most impressive skill"'
+    )
+
+    return TYPING_CHOICE
+
+
+def received_information(update: Update, context: CallbackContext) -> int:
+    """Записываем инфу в определенный ключ"""
+    user_data = context.user_data
+    text = update.message.text
+    category = user_data['choice']
+    user_data[category] = text
+    del user_data['choice']
+
+    update.message.reply_text(
+        "Neat! Just so you know, this is what you already told me:"
+        f"{facts_to_str(user_data)} You can tell me more, or change your opinion"
+        " on something.",
+        reply_markup=markup,
+    )
+
+    return CHOOSING
+
+
+def done(update: Update, context: CallbackContext) -> int:
+    """Выводим инфу и выключаем бота"""
+    user_data = context.user_data
+    if 'choice' in user_data:
+        del user_data['choice']
+
+    update.message.reply_text(
+        f"I learned these facts about you: {facts_to_str(user_data)}Until next time!",
+        reply_markup=ReplyKeyboardRemove(),
+    )
+
+    user_data.clear()
     return ConversationHandler.END
 
 
-def send_audio(update: Update, context: CallbackContext):
-    chat_id = update.effective_chat.id
-    url = open('AFRAID.mp4', 'rb')
-    update.message.bot.send_audio(chat_id, url)
+def main() -> None:
+    """Run the bot."""
+    # бот 
+    updater = Updater("5386827464:AAGLD9D-ypRRQJmc-LTB82EUW7VDVRmTKt0")
 
-
-def main():
-    TOKEN = '5386827464:AAGLD9D-ypRRQJmc-LTB82EUW7VDVRmTKt0'
-    updater = Updater(TOKEN)
+    # добавляем диспечер
     dispatcher = updater.dispatcher
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('start', start)],
         states={
-            KEYBOARD: [
-                MessageHandler(Filters.text, keyboard)
+            CHOOSING: [
+                MessageHandler(
+                    Filters.regex('^(Age|Favourite colour|Number of siblings)$'), regular_choice
+								),
+                MessageHandler(Filters.regex('^Something else...$'), custom_choice),
             ],
-            BUTTONS: [
-                CallbackQueryHandler(video, pattern='video'),
-                CallbackQueryHandler(audio, pattern='audio'),
-                CallbackQueryHandler(cancel, pattern='cancel')
-            ]
+            TYPING_CHOICE: [
+                MessageHandler(
+                    Filters.text & ~(Filters.command | Filters.regex('^Done$')), regular_choice
+                )
+            ],
+            TYPING_REPLY: [
+                MessageHandler(
+                    Filters.text & ~(Filters.command | Filters.regex('^Done$')),
+                    received_information,
+                )
+            ],
         },
-        fallbacks=[CommandHandler('exit', exit)])
+        fallbacks=[MessageHandler(Filters.regex('^Done$'), done)], # возвращаемся к началу бота
+    )
 
-    # filters.TEXT & (filters.Entity(URL) | filters.Entity(TEXT_LINK)) ?????
-    # filters.TEXT & (~ filters.FORWARDED)
-    # Filters.regex('^(Boy|Girl|Other)$')
-
-    dispatcher.add_handler(CommandHandler('send', send_audio))
     dispatcher.add_handler(conv_handler)
     updater.start_polling()
     updater.idle()
@@ -164,4 +151,4 @@ def main():
 
 if __name__ == '__main__':
     main()
-
+	
